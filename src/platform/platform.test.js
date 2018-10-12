bba2da80ab4f2f0d616cf7091c2e7df0d3a04d43
Lorenzo Mussi <lorenzo.mussi@docomodigital.com>
2018-10-12 14:40:12 +0200

    platform exported and tested

--- src/platform/platform.test.js
+++ src/platform/platform.test.js
@@ -1,7 +1,17 @@
 import Platform from './index';
 
 test('Platform should get Platform', () => {
-    var platform = Platform.OS;
+    const platform = Platform.OS;
+
+    expect(platform).toBe('desktop');
+});
+
+test('Platform should get Platform', () => {
+    navigator.__defineGetter__('userAgent', function(){
+        return 'Android' // customized user agent
+    });
+
+    const platform = Platform.OS;
 
     expect(platform).toBe('desktop');
 });
