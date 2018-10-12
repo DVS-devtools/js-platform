e1cde332609a1a820078a277e58eb292ced6ec11
Lorenzo Mussi <lorenzo.mussi@docomodigital.com>
2018-10-12 15:24:24 +0200

    new doc + index js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,62 @@
 /**
  * @class JsPlatform
- * @description Description of your library
+ * @description JsPlatform is a library that allow you to get platform wherever you are
+ * (iphone, ipad, android or desktop);
+ * @param {String} uaString user agent string
+ *
+ * @example
+ * const Platform = new JsPlatform();
+ *
+ * switch(Platform.OS){
+ *  case 'android':
+ *      console.log("I'm an android! ");
+ *      break;
+ *  case 'ios':
+ *      console.log("I'm an ios! ");
+ *      break;
+ *  case 'desktop':
+ *      console.log("I'm a desktop! ");
+ *      break;
+ * }
  */
-import { Platform } from './platform';
 
-export default {
+export class JsPlatform {
+    constructor(uaString = window.navigator.userAgent) {
+        this.uaString = uaString;
+    }
+
     /**
-     * @see modules/OS
+     * Get the user agent string
+     * @return {String} user agent string.
+     * @example
+     * const myUserAgent = JsPlatform.OS;
      */
-    OS: new Platform(),
-};
+    get OS() {
+        const IS_IPAD = (/iPad/i).test(this.uaString);
+        const IS_ANDROID = (/Android/i).test(this.uaString);
+
+        // The Facebook app's UIWebView identifies as both an iPhone and iPad, so
+        // to identify iPhones, we need to exclude iPads.
+        // http://artsy.github.io/blog/2012/10/18/the-perils-of-ios-user-agent-sniffing/
+
+        const IS_IPHONE = (/iPhone/i).test(this.uaString) && !IS_IPAD;
+        const IS_IPOD = (/iPod/i).test(this.uaString);
+        const IS_IOS = IS_IPHONE || IS_IPAD || IS_IPOD;
+
+        let platform = '';
+
+        switch (true) {
+        case (IS_ANDROID):
+            platform = 'android';
+            return platform;
+        case (IS_IOS):
+            platform = 'ios';
+            return platform;
+        default:
+            platform = 'desktop';
+            return platform;
+        }
+    }
+}
+
+export default new JsPlatform();
---
+++ src/platform.test.js
@@ -0,0 +1,22 @@
+import { Platform } from './';
+
+describe('Platform', () => {
+    const iPhone6 = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1';
+    const GalaxyS5 = 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Mobile Safari/537.36';
+    let platformInstance = null;
+
+    it('Platform.OS android', () => {
+        platformInstance = new Platform(GalaxyS5);
+        expect(platformInstance.OS).toEqual('android');
+    });
+
+    it('Platform.OS ios', () => {
+        platformInstance = new Platform(iPhone6);
+        expect(platformInstance.OS).toEqual('ios');
+    });
+
+    it('Platform.OS UserAgent', () => {
+        platformInstance = new Platform();
+        expect(platformInstance.OS).toEqual('desktop');
+    });
+});
