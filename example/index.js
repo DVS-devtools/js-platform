dd9aa3456705470cff97b3fe39eaa92bbfdda7da
Lorenzo Mussi <lorenzo.mussi@docomodigital.com>
2018-10-12 15:40:11 +0200

    examples completed

--- example/index.js
+++ example/index.js
@@ -1,8 +1,35 @@
-import JsPlatform from '../src/index.js';
+import {JsPlatform} from '../src/index.js';
 
-const foo = JsPlatform.init({
-    message: 'hello'
-});
+const iPhone6 = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1';
+const GalaxyS5 = 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Mobile Safari/537.36';
+const Desktop = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
 
-document.write(foo);
-console.log(foo);
+let platformInstance = new JsPlatform();
+
+
+const printUa = () => {
+    console.log('UA Changed in ' + platformInstance.OS);
+    document.getElementById('jsPlatform-example-ua').innerText = platformInstance.OS.toUpperCase();
+}
+
+window.switchIos = () => {
+    platformInstance = new JsPlatform(iPhone6)
+    printUa();
+}
+
+window.switchAndroid = () => {
+    platformInstance = new JsPlatform(GalaxyS5)
+    printUa();
+}
+
+window.switchDesktop = () => {
+    platformInstance = new JsPlatform(Desktop);
+    printUa();
+}
+
+window.currentUserAgent = () => {
+    platformInstance = new JsPlatform();
+    printUa();
+}
+
+printUa();
